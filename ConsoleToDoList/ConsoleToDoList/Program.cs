52733bec0e57b68f46cebd18d2f78c1319782bfb
MihkelPLoompuu <158562003+MihkelPLoompuu@users.noreply.github.com>
2025-05-21 16:03:45 +0300

    Add files via upload

---
+++ ConsoleToDoList/ConsoleToDoList/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleToDoList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ ConsoleToDoList/ConsoleToDoList/ToDoItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleToDoList
+{
+    public class ToDoItem
+    {
+        public string Description { get; }
+        public bool IsCompleted { get; set; }
+
+        public ToDoItem(string description)
+        {
+            Description = description;
+            IsCompleted = false;
+        }
+    }
+}
