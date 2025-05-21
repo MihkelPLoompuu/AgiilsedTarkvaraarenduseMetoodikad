74752e4d04ae07213668eb041e822e2b30d800be
MihkelPLoompuu <158562003+MihkelPLoompuu@users.noreply.github.com>
2025-05-21 16:13:04 +0300

    Add files via upload

--- ConsoleToDoList/ConsoleToDoList/Program.cs
+++ ConsoleToDoList/ConsoleToDoList/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ToDoList list = new ToDoList();
+            list.Run();
         }
     }
 }
